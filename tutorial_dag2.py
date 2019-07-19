5eb1b156920f35ecb268e3a7f01b0363f6dee4f0
yparesh <yparesh@gmail.com>
2019-07-19 02:42:55 -0400

    Update tutorial_dag2.py

--- tutorial_dag2.py
+++ tutorial_dag2.py
@@ -55,7 +55,7 @@ default_args = {
 }
 
 dag = DAG(
-    'tutorial',
+    'tutorial2',
     default_args=default_args,
     description='A simple tutorial DAG',
     schedule_interval=timedelta(days=1),
